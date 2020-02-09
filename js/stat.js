76fae3fa8bca6fc9837f6e737f4de6f936a32120
Yuri <50510523+yuradun@users.noreply.github.com>
2020-02-09 10:25:09 +0100

    fix в коде

--- js/stat.js
+++ js/stat.js
@@ -2,21 +2,16 @@
 
 var CLOUD_WIDTH = 530;
 var CLOUD_HEIGHT = 255;
-
 var CLOUD_X = 80;
 var CLOUD_Y = 30;
-
 var PlayerStatistic_WIDTH = 40;
 var PlayerStatistic_HEIGHT = 160;
 var PlayerStatistic_X = 90;
 var PlayerStatistic_Y = 100;
-
 var INDENT = 10; 
 var INDENT_TEXT = PlayerStatistic_HEIGHT + PlayerStatistic_Y + 15;
 
-
-
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
   
   for (var i = 0; i < arr.length; i++) {
@@ -28,12 +23,12 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function(ctx, players, times) {
+window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + INDENT, CLOUD_Y + INDENT, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   ctx.fillStyle = 'black';
@@ -41,9 +36,7 @@ window.renderStatistics = function(ctx, players, times) {
   ctx.fillStyle = 'black';
   ctx.fillText('Список результатов:', 85, 65);
 
-
   var maxTime = getMaxElement(times);
-
 
   for (var i = 1; i < players.length; i++) {
   	if (players[i] == 'Вы') {
